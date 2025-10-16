1564b3ab71e00e8d4cf94d9c016d141284c0862a
Vijayanand <vijayanand563@gmail.com>
2025-10-16 04:07:35 +0100

    fix: handle CORS erros on the node app

--- node-backend/src/app.ts
+++ node-backend/src/app.ts
@@ -5,8 +5,12 @@ import routes from "./routes";
 const app: Application = express();
 const port = 3000;
 
+const corsOptions = {
+    origin: 'http://geo.carenest.services'
+}
+
 // handle CORS errors
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", routes);
